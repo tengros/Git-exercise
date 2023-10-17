c068bab40ac3625bcd785eacf8f7c34d8d4be69b
tengros <johan.tengros@hotmail.com>
2023-10-17 22:07:03 +0200

    changed printouts

--- Main.java
+++ Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Testing git");
+        System.out.println("Testing git and github");
+        System.out.println("TESTING TESTING!")
     }
 }
